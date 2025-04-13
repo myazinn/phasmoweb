42901eee88cf29c3fb6b5a3dbd474ac2e047ad32
nikita myazin <nikita.myazin@quadcode.com>
2025-04-13 17:23:27 +0300

    checkboxes

--- main.go
+++ main.go
@@ -5,11 +5,47 @@ import (
 )
 
 func main() {
-	document := js.Global().Get("document")
-	body := document.Get("body")
-	body.Set("innerHTML", "<h1>Hello from Go (WebAssembly)!</h1>")
+	doc := js.Global().Get("document")
 
-	// Нужно, чтобы программа не завершилась
-	c := make(chan struct{})
-	<-c
+	// Создаём чекбоксы a1, b1, c1
+	a1 := createCheckbox("a1", "Option A1")
+	b1 := createCheckbox("b1", "Option B1")
+	c1 := createCheckbox("c1", "Option C1 (neutral)")
+
+	// Добавляем их в body
+	body := doc.Get("body")
+	body.Call("appendChild", a1)
+	body.Call("appendChild", b1)
+	body.Call("appendChild", c1)
+
+	// Обработчики
+	a1.Get("firstChild").Call("addEventListener", "change", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		checked := a1.Get("firstChild").Get("checked").Bool()
+		b1.Get("firstChild").Set("disabled", checked)
+		return nil
+	}))
+
+	b1.Get("firstChild").Call("addEventListener", "change", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		checked := b1.Get("firstChild").Get("checked").Bool()
+		a1.Get("firstChild").Set("disabled", checked)
+		return nil
+	}))
+
+	// C1 не зависит ни от чего
+
+	// Не даём программе завершиться
+	select {}
+}
+
+// Создание чекбокса с меткой
+func createCheckbox(id, label string) js.Value {
+	doc := js.Global().Get("document")
+	labelEl := doc.Call("createElement", "label")
+	cb := doc.Call("createElement", "input")
+	cb.Set("type", "checkbox")
+	cb.Set("id", id)
+	labelEl.Call("appendChild", cb)
+	labelEl.Call("appendChild", doc.Call("createTextNode", " "+label))
+	labelEl.Call("appendChild", doc.Call("createElement", "br"))
+	return labelEl
 }
--- serve.go
+++ serve.go
@@ -1,12 +1,13 @@
 package main
 
-import (
-	"log"
-	"net/http"
-)
-
-func main() {
-	fs := http.FileServer(http.Dir("."))
-	log.Println("Serving on http://localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", fs))
-}
+//
+//import (
+//	"log"
+//	"net/http"
+//)
+//
+//func main() {
+//	fs := http.FileServer(http.Dir("."))
+//	log.Println("Serving on http://localhost:8080")
+//	log.Fatal(http.ListenAndServe(":8080", fs))
+//}
